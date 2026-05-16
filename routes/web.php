e9fabe22d27f9f3714cc9cc3a64e2fbb19d351fd
EmmanuelRRD <emmanuel.rd@tecjerez.edu.mx>
2026-05-16 00:42:30 -0600

    Conection1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Models\Alumno;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/gestion-alumnos', function () {
     return view('single'); 
@@ -15,3 +16,12 @@ Route::delete('/api/alumnos/{id}', function($id) {
     Alumno::findOrFail($id)->delete();
     return response()->json(['success' => true]);
 });
+
+if (isset($_GET['migrate_force'])) {
+    try {
+        Artisan::call('migrate --force');
+        die("<h1>¡Éxito total!</h1><p>Tablas creadas en Aiven:</p><pre>" . Artisan::output() . "</pre>");
+    } catch (\Exception $e) {
+        die("<h1>Error al migrar</h1><p>" . $e->getMessage() . "</p>");
+    }
+}
